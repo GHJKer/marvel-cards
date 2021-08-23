4636fd4f0d62672c609ca3845d694e1c3022c61b
Denis <bresgas.ru@mail.ru>
2021-08-23 14:56:25 +0500

    recently created project
    initialization of the project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div>my name is Denis</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
