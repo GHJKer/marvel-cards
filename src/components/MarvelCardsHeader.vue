dc64e58f2a9610f07a242ce3ad02bfc7d8002634
Denis <bresgas.ru@mail.ru>
2021-08-24 14:11:33 +0500

    header&buttons
    created header and buttons, added styles and background image

--- src/components/MarvelCardsHeader.vue
+++ src/components/MarvelCardsHeader.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        im header!
+    <div class="header">
+        <div class="header-img">
+          <img src="https://images.vectorhq.com/images/previews/cd8/marvel-logo-psd-444569.png" alt="">  
+        </div>
+        <div class="btn-group">
+            <button>MOVIES</button>
+            <button>CHARACTERS</button>
+            <button><a href="https://www.bbc.com/news">NEWS</a></button>
+        </div>
+        <div class="reg-group">
+            <button>LOG IN</button>
+            <button>SIGN UP</button>
+        </div>
     </div>
 </template>
 
@@ -10,3 +21,48 @@ export default {
     name: 'MarvelCardsHeader'
 }
 </script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap');
+.header {
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.btn-group {
+    width: 350px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.reg-group {
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+button {
+    border: none;
+    font-family: 'Noto Sans JP', sans-serif;
+    font-weight: 700;
+    color: white;
+    background-color: transparent;
+    cursor: pointer;
+}
+
+a {
+    text-decoration: none;
+    color: white
+}
+
+img {
+    height: 50px;
+}
+
+.bd {
+    border: 1px solid red;
+}
+</style>
--- src/components/marvelCardsMain.vue
+++ src/components/marvelCardsMain.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-        <img src="https://www.awn.com/sites/default/files/styles/original/public/image/featured/1046202-sympathy-devil-how-digital-domain-helped-create-thanos-avengers-infinity-war.jpg?itok=aBpL4XP-" alt="">
-        <div class="header-component"><MarvelCardsHeader /></div>
+    <div class="container-container">
+        <div class="main-container">
+            <MarvelCardsHeader />
+        </div>
     </div>
 </template>
 
@@ -17,17 +18,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-img {
-    z-index: 0;
-}
-
-.header-component {
-    z-index: 1;
-}
-
-div {
+.main-container {
     width: 1300px;
     margin-left: auto;
     margin-right: auto;
+    min-height: 500px;
+}
+
+.container-container {
+    background: url(https://www.awn.com/sites/default/files/styles/original/public/image/featured/1046202-sympathy-devil-how-digital-domain-helped-create-thanos-avengers-infinity-war.jpg?itok=aBpL4XP-);
 }
 </style>
